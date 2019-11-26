cae0197b6a0dd274514e1f0f790186fe09789623
Elliiie <eli.georgieva.06@gmail.com>
2019-11-26 23:35:13 +0200

    authentication done

---
+++ models.py
@@ -0,0 +1,24 @@
+from flask_login import UserMixin
+from . import LoginManager
+from . import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
+    
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return self.id
+
+    def __unicode__(self):
+        return self.username
