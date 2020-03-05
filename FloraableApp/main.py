9c5beb0c79a97852cefaef5f2eda86add0262629
Elliiie <eli.georgieva.06@gmail.com>
2020-03-05 18:39:27 +0200

    added multiple client handling

--- FloraableApp/main.py
+++ FloraableApp/main.py
@@ -67,37 +67,37 @@ def deleteDevice(device, board):
 
 def on_connect(client, userdata, flags, rc):
         print("Connected with result code "+str(rc))
-        client.subscribe("/profile/4131074/esp8266/temperature")
-        client.subscribe("/profile/4131074/esp8266/humidity")
-        client.subscribe("/profile/4131074/esp8266/soilMoisture")
-        client.subscribe("/profile/4131074/esp8266/light")
+        client.subscribe("/profile/+/esp8266/temperature")
+        client.subscribe("/profile/+/esp8266/humidity")
+        client.subscribe("/profile/+/esp8266/soilMoisture")
+        client.subscribe("/profile/+/esp8266/light")
 
 def on_message(client, userdata, message):
 
     print("Received message '" + str(message.payload) + "' on topic '" + message.topic + "' with QoS " + str(message.qos))
 
-    if message.topic == "/profile/4131074/esp8266/temperature":
+    if message.topic == "/profile/+/esp8266/temperature":
         print("temperature update")
         socketio.emit('dht_temperature', {'data' : message.payload})
         new_temp_value = TemperatureSensorValue(value=message.payload, device_id=4131074)
         db.session.add(new_temp_value)
         db.session.commit()
 
-    if message.topic == "/profile/4131074/esp8266/humidity":
+    if message.topic == "/profile/+/esp8266/humidity":
         print("humidity update")
         socketio.emit('dht_humidity', {'data' : message.payload})
         new_humidity_value = HumiditySensorValue(value=message.payload, device_id=4131074)
         db.session.add(new_humidity_value)
         db.session.commit()
         
-    if message.topic == "/profile/4131074/esp8266/soilMoisture":
+    if message.topic == "/profile/+/esp8266/soilMoisture":
         print("soil moisture update")
         socketio.emit('soilMoisture_sensor', {'data' : message.payload})
         new_moisture_value = MoistureSensorValue(value=message.payload, device_id=4131074)
         db.session.add(new_moisture_value)
         db.session.commit()
 
-    if message.topic == "/profile/4131074/esp8266/light":
+    if message.topic == "/profile/+/esp8266/light":
         print("light update")
         socketio.emit('light_sensor', {'data' : message.payload})
         new_light_value = LightSensorValue(value=message.payload, device_id=4131074)
@@ -110,14 +110,12 @@ mqttc.connect("localhost", 1883, 60)
 mqttc.loop_start()
 
 pins = {
-    12 : {'name' : 'LED Strip', 'board' : 'esp8266', 'topic' : 'profile/4131074/esp8266/12', 'state' : 'False'},
-    13 : {'name' : 'Water pump', 'board' : 'esp8266', 'topic' : 'profile/4131074/esp8266/13', 'state' : 'False'}
+    12 : {'name' : 'LED Strip', 'board' : 'esp8266', 'topic' : 'profile/<string:device>/esp8266/12', 'state' : 'False'},
+    13 : {'name' : 'Water pump', 'board' : 'esp8266', 'topic' : 'profile/<string:device>/esp8266/13', 'state' : 'False'}
 }
-
 templateData = {
     'pins' : pins
 }
-
 @main.route("/profile/<string:device>/<board>", methods=['GET','POST'])
 def controllDevice(device, board):
 
@@ -137,13 +135,13 @@ def controllDevice(device, board):
             print(moistureVal)
             
             if(moistureVal.value < plant.moisture_value):
-                mqttc.publish(pins[13]['topic'], "1")
+                mqttc.publish(pins[13]['profile/<device.serialNum>/esp8266/13'], "1")
                 pins[13]['state'] = 'True'
 
             lightVal = LightSensorValue.query.filter_by(device_id=device.id).first()
 
             if(lightVal.value < plant.light_value):
-                mqttc.publish(pins[12]['topic'], "1")
+                mqttc.publish(pins[12]['profile/<device.serialNum>/esp8266/12'], "1")
                 pins[12]['state'] = 'True'
             
         else:
