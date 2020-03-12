76d022e3eca407994c51b86e67d9cf565a084506
Elliiie <eli.georgieva.06@gmail.com>
2020-03-12 16:21:43 +0200

    added non-blocking delay

--- FloraableControllerCode/mqtt_esp8266.ino/mqtt_esp8266/mqtt_esp8266.ino
+++ FloraableControllerCode/mqtt_esp8266.ino/mqtt_esp8266/mqtt_esp8266.ino
@@ -2,23 +2,26 @@
 #include <PubSubClient.h>
 #include <Wire.h>
 #include <BH1750FVI.h>
-
-BH1750FVI lightMeter(BH1750FVI::k_DevModeContLowRes);
-
 #include "DHTesp.h"
 
+BH1750FVI lightMeter(BH1750FVI::k_DevModeContLowRes);
 DHTesp dht;
-
-//003F0902
-//4131074
 
 
 const char* ssid = "Venci-SetSERVICE";
 const char* password ="20061017";
-const char* mqtt_server = "192.168.0.106";
+const char* mqtt_server = "192.168.0.100";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
+
+//003F0902
+//4131074
+
+unsigned long previousMillis = 0;  
+
+
+const long interval = 60000;  
 
 //leds
 const int ledGPIO5 = 12;
@@ -53,13 +56,10 @@ void setup_wifi() {
     Serial.print(".");
   }
 
-  randomSeed(micros());
-
   Serial.println("");
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
-  Serial.println(F("BH1750 Test"));
 }
 
 void callback(String topic, byte* message, unsigned int length) {
@@ -100,115 +100,111 @@ void callback(String topic, byte* message, unsigned int length) {
 }
 
 void reconnect() {
+  // Loop until we're reconnected
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
     if (client.connect("ESP8266Client")) {
       Serial.println("connected");  
-      
-      client.subscribe("profile/4131074/esp8266/12");
-      client.subscribe("profile/4131074/esp8266/13");
-      
+      // Subscribe or resubscribe to a topic
+      // You can subscribe to more topics (to control more LEDs in this example)
+      String topic1 = "profile/4131074/esp8266/12";
+      String topic2 = "profile/4131074/esp8266/13";
+
+      client.subscribe(topic1.c_str());
+      client.subscribe(topic2.c_str());
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
       delay(5000);
     }
   }
 }
 
 void setup() {
-  pinMode(pump, OUTPUT);
-  pinMode(ledGPIO5, OUTPUT);
-  
   Serial.begin(9600);
   setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
-
+  
+  pinMode(pump, OUTPUT);
+  pinMode(ledGPIO5, OUTPUT);
   Wire.begin(D1, D2);
   lightMeter.begin();
-  dht.setup(D5, DHTesp::DHT22); // Connect DHT sensor to GPIO 17
+  dht.setup(D5, DHTesp::DHT22);
 }
  
 
 void send_tempreture(){
-  
+    lastMeasure = now;
+    
+    float h = dht.getHumidity();
+    float t = dht.getTemperature();
+        
+    if (isnan(h) || isnan(t)) {
+      Serial.println("Failed to read from DHT sensor!");
+      return;
+    }
 
-        lastMeasure = now;
-        
-        float h = dht.getHumidity();
-        float t = dht.getTemperature();
-            
-        if (isnan(h) || isnan(t)) {
-          Serial.println("Failed to read from DHT sensor!");
-          return;
-        }
+    float hic = dht.computeHeatIndex(t, h, false);
+    static char temperatureTemp[7];
+    dtostrf(hic, 6, 2, temperatureTemp);
     
-        float hic = dht.computeHeatIndex(t, h, false);
-        static char temperatureTemp[7];
-        dtostrf(hic, 6, 2, temperatureTemp);
-        
-        static char humidityTemp[7];
-        dtostrf(h, 6, 2, humidityTemp);
+    static char humidityTemp[7];
+    dtostrf(h, 6, 2, humidityTemp);
+
+    String topicTemperature = ("/profile/4131074/esp8266/temperature");
+    client.publish(topicTemperature.c_str(), temperatureTemp);  
+
+    String topicHumidity = ("/profile/4131074/esp8266/humidity");
+    client.publish(topicHumidity.c_str(), humidityTemp);  
+
     
-        String topicTemperature = ("/profile/4131074/esp8266/temperature");
-        client.publish(topicTemperature.c_str(), temperatureTemp);  
-    
-        String topicHumidity = ("/profile/4131074/esp8266/humidity");
-        client.publish(topicHumidity.c_str(), humidityTemp);  
-    
-        
-        Serial.print("Humidity: ");
-        Serial.print(h);
-        Serial.print(" %\t Temperature: ");
-        Serial.print(t);
-        Serial.print(" *C ");
-        Serial.print(hic);
-        Serial.println(" *C ");
-    
+    Serial.print("Humidity: ");
+    Serial.print(h);
+    Serial.print(" %\t Temperature: ");
+    Serial.print(t);
+    Serial.print(" *C ");
+    Serial.print(hic);
+    Serial.println(" *C ");
 }
 
 void send_moisture(){
- 
-      inputVal = analogRead (analog); 
-      Serial.println("Soil moisture: ");
-      Serial.println(inputVal);
-      static char soilmoisture[7];
-      dtostrf(inputVal, 6, 2, soilmoisture);
-      String topicMoisture = ("/profile/4131074/esp8266/soilMoisture");
-      client.publish(topicMoisture.c_str(), soilmoisture);  
-  
+  inputVal = analogRead (analog); 
+  Serial.println("Soil moisture: ");
+  Serial.println(inputVal);
+  static char soilmoisture[7];
+  dtostrf(inputVal, 6, 2, soilmoisture);
+  String topicMoisture = ("/profile/4131074/esp8266/soilMoisture");
+  client.publish(topicMoisture.c_str(), soilmoisture);   
 }
 
 void send_light(){
-
-      uint16_t lux = lightMeter.GetLightIntensity();
-      static char lightLux[7];
-      dtostrf(lux, 6, 2, lightLux);
-      String topicLight = ("/profile/4131074/esp8266/light");
-      client.publish(topicLight.c_str(), lightLux);  
-      Serial.println("Light level: ");
-      Serial.println (lux);  
+  uint16_t lux = lightMeter.GetLightIntensity();
+  static char lightLux[7];
+  dtostrf(lux, 6, 2, lightLux);
+  String topicLight = ("/profile/4131074/esp8266/light");
+  client.publish(topicLight.c_str(), lightLux);  
+  Serial.println("Light level: ");
+  Serial.println (lux);  
 }
+
 
 void loop() {
   if (!client.connected()) {
     reconnect();
   }
-  if(!client.loop()){
+  if(!client.loop())
     Serial.print("opaaa");
     client.connect("ESP8266Client");
+
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+      previousMillis = currentMillis;
+      send_moisture();
+      send_tempreture();
+      send_light();
   }
-  
-    send_tempreture();
-    send_moisture();
-    send_light();
-    
-    delay(60000);
-      
-    //Serial.println("id: ");
-    //Serial.println(ESP.getChipId());
-    //delay(2000);
-    
 }
